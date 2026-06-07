60e1413f8867bb8d814f12c9e371aa54c504d703
Makhosonke Morafo <makhosonke@komokun.org>
2026-06-07 08:18:40 +0200

    fix: update

--- python/fwd_client/__init__.py
+++ python/fwd_client/__init__.py
@@ -9,7 +9,7 @@ Public API::
 
 from __future__ import annotations
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from fwd_client.client import FwdClient
 from fwd_client.errors import FwdError, FwdRetryableError, FwdTerminalError, raise_for_fwd_error
